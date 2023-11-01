3eb41332b572711c394ff21eac4c1d5053257a42
DominicKepes <143762188+DominicKepes@users.noreply.github.com>
2023-10-31 19:30:30 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
+var charName = prompt("enter search term for marvel api (Character Name)")
+
 var pubKey = "81c4dc1da0eaac5d29524d7ffb947b4a"
 var timeStamp = Date.now()
 
-fetch(`https://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${pubKey}`)
+
+fetch(`https://gateway.marvel.com/v1/public/characters?name=${charName}&ts=${timeStamp}&apikey=${pubKey}`)
 .then(function(response){return response.json()})
 .then(function(response) {
   console.log(response)
 })
+characters?name=Hulk
